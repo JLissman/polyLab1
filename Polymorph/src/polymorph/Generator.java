df9c3069c6be5e2592ad60603ad078e37f89e215
Salar Loran Telo <Salarmail12@gmail.com>
2021-01-26 16:05:46 +0100

    formatering

--- Polymorph/src/polymorph/Generator.java
+++ Polymorph/src/polymorph/Generator.java
@@ -12,99 +12,98 @@ import polymorph.Employees.HR;
 import polymorph.Employees.Sales;
 import polymorph.Employees.Secretary;
 import polymorph.Employees.Technician;
+
 /**
  *
  * @author Jonathan
  */
 public class Generator {
+  
     Random RNG = new Random();
     int index;
-    ArrayList<String> randomNames = new ArrayList<>(Arrays.asList("Maria","Erik","Anna","Lars","Margareta","Karl","Elisabeth","Anders","Eva","Johan","Kristina","Per","Birgitta","Nils","Karin","Carl","Marie","Mikael","Elisabet","Jan","Ingrid","Hans","Christina","Peter","Sofia","Olof","Linnéa","Lennart","Kerstin","Gunnar","Lena","Sven","Helena","Fredrik","Marianne","Daniel","Emma","Bengt","Linnea","Bo","Johanna","Alexander","Inger","Gustav","Sara","Göran","Cecilia","Åke","Elin","Magnus"));
-    ArrayList<String> efterNamn = new ArrayList<>(Arrays.asList("Andersson","Johansson","Karlsson","Nilsson","Eriksson","Larsson","Olsson","Persson","Svensson","Gustafsson","Pettersson","Jonsson","Jansson","Hansson","Bengtsson","Jönsson","Lindberg","Jakobsson","Magnusson","Olofsson","Lindström","Lindqvist","Lindgren","Axelsson","Berg","Bergström","Lundberg","Lind","Lundgren","Lundqvist","Mattsson","Berglund","Fredriksson","Sandberg","Henriksson","Forsberg","Sjöberg","Wallin","Ali","Engström","Mohamed","Eklund","Danielsson","Lundin","Håkansson","Björk","Bergman","Gunnarsson","Holm","Wikström","Samuelsson","Isaksson","Fransson","Bergqvist","Nyström","Holmberg","Arvidsson","Löfgren","Söderberg","Nyberg","Blomqvist","Claesson","Nordström","Mårtensson","Lundström","Ahmed","Viklund","Björklund","Eliasson","Pålsson","Hassan","Berggren","Sandström","Lund","Nordin","Ström","Åberg","Hermansson","Ekström","Falk","Holmgren","Dahlberg","Hellström","Hedlund","Sundberg","Sjögren","Ek","Blom","Abrahamsson","Martinsson","Öberg","Andreasson","Strömberg","Månsson","Åkesson","Hansen","Norberg","Lindholm","Dahl","Jonasson"));
-    public String Name(){
+    ArrayList<String> randomNames = new ArrayList<>(Arrays.asList("Maria", "Erik", "Anna", "Lars", "Margareta", "Karl", "Elisabeth", "Anders", "Eva", "Johan", "Kristina", "Per", "Birgitta", "Nils", "Karin", "Carl", "Marie", "Mikael", "Elisabet", "Jan", "Ingrid", "Hans", "Christina", "Peter", "Sofia", "Olof", "Linnéa", "Lennart", "Kerstin", "Gunnar", "Lena", "Sven", "Helena", "Fredrik", "Marianne", "Daniel", "Emma", "Bengt", "Linnea", "Bo", "Johanna", "Alexander", "Inger", "Gustav", "Sara", "Göran", "Cecilia", "Åke", "Elin", "Magnus"));
+    ArrayList<String> efterNamn = new ArrayList<>(Arrays.asList("Andersson", "Johansson", "Karlsson", "Nilsson", "Eriksson", "Larsson", "Olsson", "Persson", "Svensson", "Gustafsson", "Pettersson", "Jonsson", "Jansson", "Hansson", "Bengtsson", "Jönsson", "Lindberg", "Jakobsson", "Magnusson", "Olofsson", "Lindström", "Lindqvist", "Lindgren", "Axelsson", "Berg", "Bergström", "Lundberg", "Lind", "Lundgren", "Lundqvist", "Mattsson", "Berglund", "Fredriksson", "Sandberg", "Henriksson", "Forsberg", "Sjöberg", "Wallin", "Ali", "Engström", "Mohamed", "Eklund", "Danielsson", "Lundin", "Håkansson", "Björk", "Bergman", "Gunnarsson", "Holm", "Wikström", "Samuelsson", "Isaksson", "Fransson", "Bergqvist", "Nyström", "Holmberg", "Arvidsson", "Löfgren", "Söderberg", "Nyberg", "Blomqvist", "Claesson", "Nordström", "Mårtensson", "Lundström", "Ahmed", "Viklund", "Björklund", "Eliasson", "Pålsson", "Hassan", "Berggren", "Sandström", "Lund", "Nordin", "Ström", "Åberg", "Hermansson", "Ekström", "Falk", "Holmgren", "Dahlberg", "Hellström", "Hedlund", "Sundberg", "Sjögren", "Ek", "Blom", "Abrahamsson", "Martinsson", "Öberg", "Andreasson", "Strömberg", "Månsson", "Åkesson", "Hansen", "Norberg", "Lindholm", "Dahl", "Jonasson"));
+
+    public String Name() {
         index = RNG.nextInt(50);
         String fname = randomNames.get(index);
         int indexTwo = RNG.nextInt(100);
         String lname = efterNamn.get(indexTwo);
-        return fname+" "+lname;
+        return fname + " " + lname;
     }
-    
-    
-    public int Age(){
+    public int Age() {
         int min = 25, result = 0;
-        while (result < min){
-        result = RNG.nextInt(65);
+        while (result < min) {
+            result = RNG.nextInt(65);
         }
-               return result;
+        return result;
     }
-    
-    public String Sex(String Name){
+    public String Sex(String Name) {
         int index = randomNames.indexOf(Name);
-        
-        
-        if (index % 2 == 0){
-        return "Female";
-        }else
-        {
-        return "Male";
+
+        if (index % 2 == 0) {
+            return "Female";
+        } else {
+            return "Male";
         }
     }
-    public String Position(){
-        
-    int random = RNG.nextInt(4);
-    String pos="";
-    switch(random){
-        case(0):
-            pos = "HR";
-            break;
-        case(1):
-            pos = "Sales";
-            break;
-        case(2):
-            pos = "Secretary";
-            break;
-        case(3):
-            pos = "Technician";
-            break;
-        default:
-            break;
+
+    public String Position() {
+
+        int random = RNG.nextInt(4);
+        String pos = "";
+        switch (random) {
+            case (0):
+                pos = "HR";
+                break;
+            case (1):
+                pos = "Sales";
+                break;
+            case (2):
+                pos = "Secretary";
+                break;
+            case (3):
+                pos = "Technician";
+                break;
+            default:
+                break;
+        }
+        return pos;
     }
-    return pos;
-    }
-    
-    
-    public int Salary(String position){
-        int min = 20000, salary=0;
+
+
+    public int Salary(String position) {
+        int min = 20000, salary = 0;
         switch (position) {
             case "HR":
-                while (salary < min){
+                while (salary < min) {
                     salary = RNG.nextInt(35000);
                 }
                 break;
             case "Sales":
-                while (salary < min){
+                while (salary < min) {
                     salary = RNG.nextInt(40000);
                 }
                 break;
             case "Secretary":
-                while (salary < min){
+                while (salary < min) {
                     salary = RNG.nextInt(30000);
                 }
                 break;
             case "Technician":
-                while (salary < min){
+                while (salary < min) {
                     salary = RNG.nextInt(35000);
                 }
                 break;
             default:
                 break;
         }
-    return salary;
+        return salary;
     }
 
-    public EmployeeManagement Business(int workers){
-    EmployeeManagement business = new EmployeeManagement();
-    Generator gen = new Generator();
+    public EmployeeManagement Business(int workers) {
+        EmployeeManagement business = new EmployeeManagement();
+        Generator gen = new Generator();
         int x = RNG.nextInt(200);
         for (int i = 0; i < workers; i++) {
             String name = gen.Name();
@@ -128,13 +127,10 @@ public class Generator {
                 business.AddEmployee(e1);
 
             }
-            
 
         }
-    return business;
+        return business;
     }
 
+    
 }
-
-
-
